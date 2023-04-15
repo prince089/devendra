027c4e5684f972f7aed01785e2b7bafdc808b8b2
prince089 <prince.hirapara890@gmail.com>
2023-04-15 19:00:46 +0530

    init

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,3 @@
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+//String API_KEY = "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";
+String API_KEY ="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";
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Flutter ChatBOT',
+        title: 'ChatAPI Bot ',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
             scaffoldBackgroundColor: scaffoldBackgroundColor,
